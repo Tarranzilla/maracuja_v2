37da2e358507e72c1647f11efd48ae2fb2ff6ba1
JOAO TARRAN <joaotarran@gmail.com>
2024-05-06 11:09:13 -0300

    Cookies and Loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,51 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import React, { useState, useEffect } from "react";
+
+import { motion as m, AnimatePresence } from "framer-motion";
+
+import Image from "next/image";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isCookiesAccepted, setIsCookiesAccepted] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoaded(true);
+        }, 4000); // 2 seconds
+
+        return () => clearTimeout(timer); // Clean up on component unmount
+    }, []);
+
+    return (
+        <>
+            <AnimatePresence>
+                {!isLoaded && (
+                    <m.div initial={{ opacity: 1 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="Main_Loader" key={"Main_Loader"}>
+                        <Image className="Loader_Image" src="/brand_assets/Maracuja_Icon_Logo.png" alt="Loader" width={100} height={100} priority />
+                    </m.div>
+                )}
+
+                {!isCookiesAccepted && (
+                    <m.div initial={{ y: 0 }} animate={{ y: 0 }} exit={{ y: "7vh" }} className="Cookies_Banner_Container" key={"Cookies_Banner"}>
+                        <span className="material-icons Cookies_Icon">cookie</span>
+                        <p className="Cookies_Banner_Text">
+                            This website uses cookies to enhance the user experience. By using this website, you agree to our use of cookies.
+                        </p>
+                        <button
+                            className="Cookies_Banner_Btn"
+                            onClick={() => {
+                                setIsCookiesAccepted(true);
+                            }}
+                        >
+                            Got it!
+                        </button>
+                    </m.div>
+                )}
+            </AnimatePresence>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
